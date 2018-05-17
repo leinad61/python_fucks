9fab98329b9d4f1d5f7ca7e37df7ddf621754900
leinad61 <39384217+leinad61@users.noreply.github.com>
2018-05-17 21:21:30 +0200

    Add files via upload

--- ToK Question.py
+++ ToK Question.py
@@ -10,17 +10,26 @@ area_of_knowledge = ["Natural sciences", "Mathematics", "Ethics", "the Arts", \
                     "Human Sciences", "History"]
 randomizerA = random.choice(area_of_knowledge)
 
-question_starters = ["To what extent does ", "How does " + randomizerW + fill_two \
-                    ,"What is the role of " \
+question_starters = ["To what extent does ", "How does ", "What is the role of " \
                     ,"Evaluate the strengths and weaknesses of ", "Compare and contrast " \
                     ,"How can " + randomizerW + "be used to justify " \
                     ,"How can " + randomizerW + "be used to explain " \
                     ,"What is the validity of " + randomizerW + "to justify "]
 
-randomizerQ = random.choice(question_starters)
+knowledge_bank_Q1 = question_starters[0:1]
+randomizerQ1 = random.choice(knowledge_bank_Q1)
+knowledge_question1 = randomizerQ1 + randomizerW + fill_two + randomizerA
 
-##repeat = [s for s in question_starters if randomizerW in s]
+knowledge_bank_Q2 = question_starters[2:4]
+randomizerQ2 = random.choice(knowledge_bank_Q2)
+knowledge_question2 = randomizerQ2 + randomizerW + fill_one + randomizerA
 
-knowledge_question = randomizerQ + randomizerW + fill_one + randomizerA
+knowledge_bank_Q3 = question_starters[5:7]
+randomizerW3 = random.choice(way_of_knowing)
+randomizerQ3 = random.choice(knowledge_bank_Q3)
+knowledge_question3 = randomizerQ3 + randomizerW3 + fill_one + randomizerA
 
-print knowledge_question
+final_random_question_bank = [knowledge_question1, knowledge_question2, knowledge_question3]
+final_random_question = random.choice(final_random_question_bank)
+
+print final_random_question
