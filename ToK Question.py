9df0680af72aa644ad888f6d901d1c7fd1169146
leinad61 <39384217+leinad61@users.noreply.github.com>
2018-05-18 10:52:16 +0200

    Add files via upload

--- ToK Question.py
+++ ToK Question.py
@@ -1,36 +1,48 @@
 import random
 
-way_of_knowing = ["Language ", "Emotion ", "Perception ", "Reason ", "Faith " \
-                 , "Intuition ", "Memory "]
-randomizerW = random.choice(way_of_knowing)
+running = True
 
-fill_one = "in "
-fill_two = "affect "
+while running:
+    way_of_knowing = ["Language ", "Emotion ", "Perception ", "Reason ", "Faith " \
+                     , "Intuition ", "Memory "]
+    randomizerW = random.choice(way_of_knowing)
 
-area_of_knowledge = ["Natural sciences", "Mathematics", "Ethics", "the Arts", \
-                    "Human Sciences", "History"]
-randomizerA = random.choice(area_of_knowledge)
+    fill_one = "in "
+    fill_two = "affect "
+    mark = "?"
 
-question_starters = ["To what extent does ", "How does ", "What is the role of " \
-                    ,"Evaluate the strengths and weaknesses of ", "Compare and contrast " \
-                    ,"How can " + randomizerW + "be used to justify " \
-                    ,"How can " + randomizerW + "be used to explain " \
-                    ,"What is the validity of " + randomizerW + "to justify "]
+    area_of_knowledge = ["Natural sciences", "Mathematics", "Ethics", "the Arts", \
+                        "Human Sciences", "History"]
+    randomizerA = random.choice(area_of_knowledge)
 
-knowledge_bank_Q1 = question_starters[0:1]
-randomizerQ1 = random.choice(knowledge_bank_Q1)
-knowledge_question1 = randomizerQ1 + randomizerW + fill_two + randomizerA
+    question_starters = ["To what extent does ", "How does ", "What is the role of " \
+                        ,"Evaluate the strengths and weaknesses of ", "Compare and contrast " \
+                        ,"How can " + randomizerW + "be used to justify " \
+                        ,"How can " + randomizerW + "be used to explain " \
+                        ,"What is the validity of " + randomizerW + "to justify "]
 
-knowledge_bank_Q2 = question_starters[2:4]
-randomizerQ2 = random.choice(knowledge_bank_Q2)
-knowledge_question2 = randomizerQ2 + randomizerW + fill_one + randomizerA
+    knowledge_bank_Q1 = question_starters[0:1]
+    randomizerQ1 = random.choice(knowledge_bank_Q1)
+    knowledge_question1 = randomizerQ1 + randomizerW + fill_two + randomizerA + mark
 
-knowledge_bank_Q3 = question_starters[5:7]
-randomizerW3 = random.choice(way_of_knowing)
-randomizerQ3 = random.choice(knowledge_bank_Q3)
-knowledge_question3 = randomizerQ3 + randomizerW3 + fill_one + randomizerA
+    knowledge_bank_Q2 = question_starters[2:4]
+    randomizerQ2 = random.choice(knowledge_bank_Q2)
+    knowledge_question2 = randomizerQ2 + randomizerW + fill_one + randomizerA + mark
 
-final_random_question_bank = [knowledge_question1, knowledge_question2, knowledge_question3]
-final_random_question = random.choice(final_random_question_bank)
+    knowledge_bank_Q3 = question_starters[5:7]
+    way_of_knowing.remove(randomizerW)
+    randomizerW3 = random.choice(way_of_knowing)
+    way_of_knowing.append(randomizerW)
+    randomizerQ3 = random.choice(knowledge_bank_Q3)
+    knowledge_question3 = randomizerQ3 + randomizerW3 + fill_one + randomizerA + mark
+    final_random_question_bank = [knowledge_question1, knowledge_question2, knowledge_question3]
+    final_random_question = random.choice(final_random_question_bank)
 
-print final_random_question
+    print final_random_question
+
+    test = raw_input("Do you want to run again: ")
+    no = ["NO", "No", "no", "N", "n"]
+    if test in no:
+        running = False
+    elif test == "nO":
+        print "Your retrad but..."
